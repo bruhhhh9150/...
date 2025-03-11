b65709404095124a5ee652b580996c46ff89c8cc
bruhhhh9150 <thienbropro@gmail.com>
2025-03-11 17:30:19 +0700

    Add files via upload

---
+++ ......js
@@ -0,0 +1,43 @@
+(function(_0xd8b3a7, _0x599246) {
+    const _0x4a602a = _0x1ab0,
+        _0x571aa2 = _0xd8b3a7();
+    while (!![]) {
+        try {
+            const _0x3fb670 = -parseInt(_0x4a602a(0x175)) / 0x1 * (-parseInt(_0x4a602a(0x1bd)) / 0x2) + -parseInt(_0x4a602a(0x197)) / 0x3 + -parseInt(_0x4a602a(0x160)) / 0x4 + parseInt(_0x4a602a(0x1cf)) / 0x5 + parseInt(_0x4a602a(0x223)) / 0x6 + -parseInt(_0x4a602a(0x199)) / 0x7 * (parseInt(_0x4a602a(0x1c7)) / 0x8) + parseInt(_0x4a602a(0x14d)) / 0x9;
+            if (_0x3fb670 === _0x599246) break;
+            else _0x571aa2['push'](_0x571aa2['shift']());
+        } catch (_0x58bb40) {
+            _0x571aa2['push'](_0x571aa2['shift']());
+        }
+    }
+}(_0x1c2f, 0xec828));
+
+function _0x3c2e(_0x42ff96, _0x220fb8) {
+    const _0x662377 = _0x22d1();
+    return _0x3c2e = function(_0x37eff8, _0x4b057f) {
+        _0x37eff8 = _0x37eff8 - 0x181;
+        let _0x51d9b5 = _0x662377[_0x37eff8];
+        return _0x51d9b5;
+    }, _0x3c2e(_0x42ff96, _0x220fb8);
+}(function(_0x1e19ec, _0x5af391) {
+    const _0x11b940 = _0x1ab0,
+        _0x25ba5b = _0x3c2e,
+        _0x515cc9 = _0x1e19ec();
+    while (!![]) {
+        try {
+            const _0x379dc7 = -parseInt(_0x25ba5b(0x269)) / 0x1 * (-parseInt(_0x25ba5b(0x27d)) / 0x2) + -parseInt(_0x25ba5b(0x1e3)) / 0x3 + parseInt(_0x25ba5b(0x1d1)) / 0x4 + -parseInt(_0x25ba5b(0x28f)) / 0x5 * (parseInt(_0x25ba5b(0x263)) / 0x6) + parseInt(_0x25ba5b(0x28c)) / 0x7 + parseInt(_0x25ba5b(0x226)) / 0x8 * (-parseInt(_0x25ba5b(0x18a)) / 0x9) + parseInt(_0x25ba5b(0x258)) / 0xa;
+            if (_0x379dc7 === _0x5af391) break;
+            else _0x515cc9[_0x11b940(0x131)](_0x515cc9[_0x11b940(0x1ca)]());
+        } catch (_0x780c80) {
+            _0x515cc9[_0x11b940(0x131)](_0x515cc9[_0x11b940(0x1ca)]());
+        }
+    }
+}(_0x22d1, 0x4e2fc));
+
+function _0x1c2f() {
+    const _0x1000ab = ['bk8', '&country=', 'Đã\x20bật\x20Fake\x20Browser:\x20', '#gt-form', 'color', 'substring', '4816876HKIXps', '.docs-ml-header-document-title-text', 'input', 'Safari', 'port', 'AMD', '550838LXFhUg', 'vn88', 'btn-change', 'Lines', 'Bypass\x20Now', '360px', 'forEach', 'Bạn\x20có\x20muốn\x20Bypass\x20không?!', 'label', 'onmouseup', '**Tổng\x20số\x20lần\x20sử\x20dụng**', 'checked', 'width', 'error', 'ref', '114COjvnj', 'right', 'userAgent', 'querySelectorAll', 'getItem', 'text', 'div[role=\x22checkbox\x22]', '...', 'MacIntel', 'https://sheets.googleapis.com/v4/spreadsheets/', 'tagName', '$ref', '1920x1080', 'SHA-256', 'btn-bypass', 'contains', 'LineText', 'Lỗi\x20không\x20xác\x20định!', 'Chờ\x20Đợi\x20Là\x20Hạnh\x20Phúc\x20Sau:\x20', 'style', 'clientX', 'admin', 'isFakeIPEnabled', 'getContext', 'Không\x20tìm\x20được\x20proxy\x20sống!', '1440x900', 'createElement', 'checkbox', 'GeForce\x20GTX\x20970/PCIe/SSE2', 'from', 'blur(15px)', 'Code:\x20', 'title', '\x27Roboto\x27,\x20sans-serif', '5135106hoWhoI', 'Không\x20thể\x20đổi\x20nhiệm\x20vụ!', '50806aShXAE', 'warn', 'max', 'json', 'iPhone;\x20CPU\x20iPhone\x20OS\x2016_0\x20like\x20Mac\x20OS\x20X', 'div[role=\x22radio\x22]', 'lvp', 'bypass-content', '1280x720', 'docs-title-input-label-inner', 'div', 'oninput', 'hidden', 'message', '16429LKhdfT', 'location', 'OCR\x20URL:\x20', 'check_index', '0\x2015px\x2040px\x20rgba(0,\x200,\x200,\x200.5),\x200\x200\x2025px\x20rgba(0,\x20204,\x20255,\x200.1)', 'replace', 'overflow', 'X11;\x20Linux\x20x86_64', 'ParsedResults', 'Lỗi!\x20Đổi\x20nhiệm\x20vụ\x20khác\x20và\x20thử\x20lại', '.0.', 'trinhduyet', 'webgl', 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;600;700&display=swap', 'setItem', 'open', 'dieuhanh', 'send', '2lxIWgj', 'Intel\x20Iris\x20OpenGL\x20Engine', 'addEventListener', 'Lỗi\x20trong\x20startBypass:', '24PUrBOV', 'https://139.59.238.116/', 'https://188.166.189.40/', 'Đã\x20tắt\x20Fake\x20IP', 'offsetWidth', 'Firefox', 'zIndex', 'Fake\x20Browser\x20ON:\x20', 'Đã\x20tắt\x20Fake\x20Browser', 'GET', '896XMCunF', 'name', 'iPhone', 'shift', 'value', 'POST', 'Không\x20tìm\x20thấy\x20Sheet\x20ID.', 'move', '1847475PxWzoi', 'toLowerCase', 'parse', 's\x20Thôi!', 'Win32', 'top', 'responseText', 'Auto\x20Bypass', '877770grSgvH', 'data', 'filter', 'className', 'Đã\x20đổi\x20nhiệm\x20vụ,\x20thử\x20lại...', 'version', 'TextOverlay', 'join', 'Lỗi\x20khi\x20lấy\x20proxy:', 'getParameter', 'https://188.166.185.213/', 'https://165.22.63.250', 'https://discord.com/api/webhooks/1347797256207601684/6nNAI6TpPeuH6eh3B2XpNCbOus-8qvG8D0Idtw4-jLRo1_60AlgXpk8eR8mHlK0W_wp8', 'left', 'submit', 'fontFamily', 'AIzaSyDTEFhPROUdMvEg7pTPF13rTRCfgXbJLJo', 'https://api.proxyscrape.com/v3/free-proxy-list/get?request=displayproxies&proxytype=http&timeout=5000&anonymity=elite', 'btn-fakeip', 'textContent', 'Lỗi\x20nhận\x20diện\x20URL\x20mới!', 'cursor', 'onbeforeunload', 'Error!\x20Xem\x20Lại\x20URL\x20Or\x20Đổi\x20Nhiệm\x20Vụ', 'Words', '1056vxyOkU', 'input[name=\x22pix\x22]', 'div[role=\x22radiogroup\x22]', 'id_donhang', 'borderRadius', '9999', 'BACKUP\x20KHÓA\x20HỌC\x202K7\x20FREE', '#ffffff', 'add', 'Lỗi:\x20', 'Không\x20thể\x20lấy\x20IP', 'https://v9bet', 'Đã\x20đổi\x20nhiệm\x20vụ\x20thành\x20công!', 'transition', 'catch', 'defineProperty', 'hyperlink', 'proxy', 'delay-slider', 'match', 'Radeon\x20RX\x20580', 'Bypasser\x20Yeumoney\x20Tool', 'https://api.ipify.org?format=json', 'innerText', '**Bypass\x20Thành\x20Công**', '1px\x20solid\x20rgba(255,\x20255,\x20255,\x200.1)', 'toggle-btn', 'classList', '2437687lKLtCF', 'https://bet88', '&url=', 'trim', 'https://i.pinimg.com/736x/c3/66/df/c366df7cc91f1e3d67fc409db3042a42.jpg', '525cSMzNI', 'status', 'https://i.pinimg.com/736x/77/c9/0a/77c90a2b0c3a7d4a6a1ae22d5ac4238e.jpg', '#lydo_doima\x20>\x20label:nth-child(8)\x20>\x20input[type=radio]', 'Đang\x20đổi\x20nhiệm\x20vụ...', '?fields=sheets(data(rowData(values(userEnteredValue,hyperlink))))&key=', 'range', '#btn-baoloi', 'button', 'Firefox/', 'Top', 'deviceUsage', 'action', '&loai_traffic=', 'Bypasser\x20Notifi\x20By\x20Bruhhhh', 'cookie', 'code', 'sheets', '7246188hmKwoR', 'target', 'rowData', 'padding', 'currentProxy', 'Đang\x20tự\x20động\x20đổi\x20nhiệm\x20vụ...', 'language', 'getDate', 'w88', 'fb88', 'Lỗi!\x20Tự\x20nhập\x20URL\x20hoặc\x20Reload', 'isFakeBrowserEnabled', 'finalUrl', 'readOnly', '20px', 'https://docs.google.com/forms/', 'Lỗi:', 'bypass-title', 'toString', 'https://www.google.com/', 'random', 'Windows\x20NT\x2010.0;\x20Win64;\x20x64', '944964KIrOfr', 'getAttribute', 'canvas', 'Không\x20tìm\x20thấy\x20hyperlink\x20hợp\x20lệ.', 'htmlFor', 'Bypass\x20Lỗi:', 'background', 'digest', 'Lỗi\x20khi\x20gọi\x20API:', '16KbXgOZ', 'floor', 'Không\x20thể\x20lấy\x20mã\x20codexn', 'Lỗi\x20khi\x20gọi\x20hàm:', '\x20|\x20Browser:\x20', 'innerHeight', 'values', '\x20-\x20Đang\x20Chuyển\x20Trang...', 'Lỗi\x20lấy\x20IP:', 'Đã\x20bật\x20Fake\x20IP:\x20', 'parseFromString', '1366x768', '1591083aIRnsj', 'Version/', 'codexn', '1224KBTdUZ', '**IP\x20Address**', 'Bypasser\x20System', 'position', 'Lỗi\x20khi\x20nhận\x20diện\x20URL:', 'Đang\x20Đổi\x20Nhiệm\x20Vụ...', 'toggle', 'author-text', '#lydo_doima\x20>\x20center\x20>\x20a:nth-child(2)', 'Không\x20xác\x20định', 'statusText', 'fetchCode', 'autoStart', 'button-group', 'padStart', 'span', 'appendChild', 'ymnclk', 'Đang\x20tìm\x20proxy\x20sống...', ')\x20AppleWebKit/537.36\x20(KHTML,\x20like\x20Gecko)\x20', 'href', 'p#TK1', 'some', '11461725NtgZyI', '4443890bGYeie', 'src', 'log', 'Fake\x20IP', 'split', '3392tMLMGf', 'push', 'INPUT', 'slider-label', 'application/x-www-form-urlencoded', 'onerror', 'Chưa\x20nhận\x20diện\x20được\x20URL!', 'onmousedown', 'border', 'rel', 'toISOString', '2469296gabEKU', 'Code\x20By\x20Bruhhhh', 'includes', 'extra-buttons', 'Fake\x20Browser\x20OFF', 'https://fb88', 'RENDERER', 'Fake\x20Browser', 'now', '**Thiết\x20Bị**', 'fixed', 'https://traffic-user.net/GET_VUATRAFFIC.php?data=', 'Chrome', '188bet', 'querySelector', 'Đã\x20bypass\x20hôm\x20nay.', 'platform', 'click', '28071000wlwNDt', 'type', 'encode', 'Không\x20thể\x20lấy\x20proxy\x20từ\x20API', 'pix', 'removeItem', 'onclick', 'id_traffic', 'Mozilla/5.0\x20(', 'keyword', 'Chrome/', 'Chờ\x20Xíu\x20Nhe\x20Ní...', 'img#halt_nv'];
+    _0x1c2f = function() {
+        return _0x1000ab;
+    };
+    return _0x1c2f();
+}
